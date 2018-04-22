5a9ec24725fa55ada5aa53dfa1b9d841b3e22b3f
Lexou <lexou-dit-tuc@hotmail.fr>
2018-04-22 05:38:32 +0200

    ENFIN UN TEST QUI MARCHE

--- main.py
+++ main.py
@@ -1,11 +1,125 @@
 import sys, pygame
 from pygame import *
-import spritesheet
 from player import *
 
 size = (win_width, win_height) = (256, 224)
 
 bgcolor = (69, 69, 69)
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Golf Rush")
+framerate = 60
+timer = pygame.time.Clock()
+
+tile_size = 16
+level_size = (level_width, level_height) = (32 * tile_size, 16 * tile_size)
+level = [
+"P                              P",
+"P                              P",
+"P                            PPP",
+"P                              P",
+"P                              P",
+"P                         P    P",
+"P      P                  P    P",
+"P     PPP           PP    P    P",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPP  P",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPP  P",
+"P                     P        P",
+"P                P             P",
+"P          P                   P",
+"P                              P",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP"]
+
+input_down  = False
+input_left  = False
+input_up    = False
+input_right = False
+input_A     = False
+input_B     = False
+
+
+def get_input():
+    global input_down
+    global input_left
+    global input_up
+    global input_right
+    global input_A
+    global input_B
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            sys.exit()
+        if e.type == KEYDOWN:
+            if e.key in (K_ESCAPE, K_q):
+                sys.exit()
+            if e.key == K_UP:
+                input_up = True
+            if e.key == K_DOWN:
+                input_down = True
+            if e.key == K_LEFT:
+                input_left = True
+            if e.key == K_RIGHT:
+                input_right = True
+            if e.key == K_SPACE:
+                input_A = True
+            if e.key == K_LALT:
+                input_B = True
+        if e.type == KEYUP:
+            if e.key == K_UP:
+                input_up = False
+            if e.key == K_DOWN:
+                input_down = False
+            if e.key == K_LEFT:
+                input_left = False
+            if e.key == K_RIGHT:
+                input_right = False
+            if e.key == K_SPACE:
+                input_A = False
+            if e.key == K_LALT:
+                input_B = False
+
+def main():
+    camera = Camera(level_width, level_height)
+
+    tiles = pygame.sprite.Group()
+    platform_x = 0
+    platform_y = 0
+    for level_x in level:
+        for level_y in level_x:
+            if level_y == 'P':
+                p = Platform(platform_x, platform_y)
+                tiles.add(p)
+            platform_x += tile_size
+        platform_x = 0
+        platform_y += tile_size
+
+    player = Player(20, 20)
+    entities = pygame.sprite.Group()
+    entities.add(player)
+
+    while True:
+        get_input()
+
+        player.update(tiles, level_width, level_height,
+            input_down,
+            input_left,
+            input_up,
+            input_right,
+            input_A,
+            input_B)
+        camera.update(player)
+
+        screen.fill(bgcolor)
+        for e in tiles:
+            screen.blit(e.image, camera.apply(e))
+        for e in entities:
+            screen.blit(e.image, camera.apply(e))
+
+        pygame.display.update()
+        timer.tick(framerate)
+
+
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self):
@@ -14,9 +128,9 @@ class Sprite(pygame.sprite.Sprite):
 class Platform(Sprite):
     def __init__(self, x, y):
         Sprite.__init__(self)
-        self.image = pygame.Surface((16, 16))
+        self.image = pygame.Surface((tile_size, tile_size))
         self.image.fill(0xFFFFFF)
-        self.rect = pygame.Rect(x, y, 16, 16)
+        self.rect = pygame.Rect(x, y, tile_size, tile_size)
 
 class Camera(object):
     #camera is a rectangle describing the coordinates of the window within the world space
@@ -46,111 +160,7 @@ class Camera(object):
             ycoord = -(level.height-win_height)
         return pygame.Rect(xcoord, ycoord, xlength, ylength)
 
-framerate = 60
-timer = pygame.time.Clock()
 
-levelSize = (levelWidth, levelHeight) = (512, 224)
-level = [
-"P                              P",
-"P                              P",
-"P                            PPP",
-"P                              P",
-"P                              P",
-"P                         P    P",
-"P     P                   P    P",
-"P    PPP            PP    P    P",
-"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-"P                              P",
-"P                              P",
-"P                              P",
-"P                              P"]
-
-input_down  = False
-input_left  = False
-input_up    = False
-input_right = False
-input_A     = False
-input_B     = False
-
-
-def get_input():
-    global input_down
-    global input_left
-    global input_up
-    global input_right
-    global input_A
-    global input_B
-    input_down  = False
-    input_left  = False
-    input_up    = False
-    input_right = False
-    input_A     = False
-    input_B     = False
-    for e in pygame.event.get():
-        if e.type == QUIT:
-            sys.exit()
-        if e.type == KEYDOWN:
-            if e.key in (K_ESCAPE, K_q):
-                sys.exit()
-            if e.key == K_UP:
-                input_up = True
-            if e.key == K_DOWN:
-                input_down = True
-            if e.key == K_LEFT:
-                input_left = True
-            if e.key == K_RIGHT:
-                input_right = True
-            if e.key == K_SPACE:
-                input_A = True
-            if e.key == K_LALT:
-                input_B = True
-
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode(size, 0, 16)
-    pygame.display.set_caption("Golf Rush")
-    camera = Camera(levelWidth, levelHeight)
-    tigersheet = spritesheet.spritesheet("tiger.png")
-
-    tiles = pygame.sprite.Group()
-    platform_x = 0
-    platform_y = 0
-    for level_x in level:
-        for level_y in level_x:
-            if level_y == "P":
-                p = Platform(platform_x, platform_y)
-                tiles.add(p)
-            platform_x += 16
-        platform_x = 0
-        platform_y += 16
-
-    entities = pygame.sprite.Group()
-    player = Player(20, 20, tigersheet)
-    entities.add(player)
-    #entities.add(player sprite ?)
-
-    while True:
-        get_input()
-
-        player.update(
-            input_down,
-            input_left,
-            input_up,
-            input_right,
-            input_A,
-            input_B)
-        player.check_collisions(tiles, levelWidth, levelHeight)
-        camera.update(player)
-
-        screen.fill(bgcolor)
-        for e in tiles:
-            screen.blit(e.image, camera.apply(e))
-        for e in entities:
-            screen.blit(e.image, camera.apply(e))
-
-        pygame.display.update()
-        timer.tick(framerate)
 
 if(__name__ == "__main__"):
     main()
--- player.py
+++ player.py
@@ -1,33 +1,53 @@
 from balls import *
 from pyganim import *
+import spritesheet
+
+framerate = 1 / (1000/60)
 
 alpha = (211, 249, 188) # 0xD3F9BC
 
+sheet = spritesheet.spritesheet("tiger.png")
+anim_idle = sheet.image_at((44,86,64,64), alpha)
+anim_jump = sheet.image_at((44,216,64,64), alpha)
+anim_walk = PygAnimation([
+            (sheet.image_at(( 44, 151, 64, 64), alpha), 0.1),
+            (sheet.image_at((109, 151, 64, 64), alpha), 0.1),
+            (sheet.image_at((174, 151, 64, 64), alpha), 0.1),
+            (sheet.image_at((239, 151, 64, 64), alpha), 0.1),
+            (sheet.image_at((304, 151, 64, 64), alpha), 0.1),
+            (sheet.image_at((369, 151, 64, 64), alpha), 0.1)])
+
+from enum import Enum
+class PlayerStatus(Enum):
+	idle = 0
+	walk = 1
+	jumpcharge = 2
+	jump = 3
+	golfcharge = 4
+	golf = 5
+	damage = 6
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, sheet):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = sheet.image_at((44,86,64,64), alpha)
-        self.rect = pygame.Rect(x + 16, y + 16, 32, 48)
-        self.pos_x = x
-        self.pos_y = y
+        self.image = anim_idle
+        self.rect = pygame.Rect(x, y, 64, 64)
+        self.hitbox = pygame.Rect(0, 0, 32, 48)
+        self.hitbox.midbottom = self.rect.midbottom
         self.vel_x = 0
         self.vel_y = 0
+        self.maxvel_x = 50
+        self.maxvel_y = 800
 
-        self.takingDamage = False
-        self.idling = True
-        self.walking = False
-        self.crouching = False
-        self.jumping = False
-        self.swinging = False
-        self.jumpswinging = False
+        self.status = PlayerStatus.idle
 
-        self.allow_jump = True
-        self.allow_swing = True
+        self.flip = False
+        self.inair = True
+        self.jumpcharge = 0
 
-        self.inair = False
-        self.chargingjump = 0
 
-    def update(self,
+
+    def update(self, tiles, level_width, level_height,
         input_down,
         input_left,
         input_up,
@@ -35,64 +55,117 @@ class Player(pygame.sprite.Sprite):
         input_A,
         input_B):
 
-        maxxvel = 100
-        maxyvel = 800
-        xvel = self.vel_x
-        yvel = self.vel_y
+        if self.status == PlayerStatus.damage:
+            pass
+        elif input_up or input_down or input_left or input_right or input_A or input_B:
+            self.walk(input_left, input_right)
+        else:
+            self.idle()
 
+        self.jump(input_A)
         if self.inair:
-            yvel += 70
-            if yvel > maxyvel:
-                yvel = maxyvel
-        if (input_left and xvel > 0) or (input_right and xvel < 0) or (input_left == input_right == False):
-            xvel = 0
-        if input_left:
-            xvel = -maxxvel
-        if input_right:
-            print("input")
-            xvel = maxxvel
-        framerate = 1 / (1000/60)
-        if input_A and not self.inair:
-            xvel = 0
-            self.chargingjump += 500 * framerate
-            if self.chargingjump > maxyvel:
-                self.chargingjump = maxyvel
-        if not input_A and self.chargingjump > 400:
-            yvel = -self.chargingjump
-            self.chargingjump = 0
+            self.fall()
 
-        self.vel_x = xvel
-        self.vel_y = yvel
+        self.update_rect(tiles, level_width, level_height)
 
-        print(xvel, yvel)
-        self.pos_x += xvel * framerate
-        self.pos_y += yvel * framerate
+        if isinstance(self.image, PygAnimation):
+            #self.elapsed = self.image.elapsed
+            self.image = self.image.getCurrentFrame()
+        if self.flip:
+            self.image = pygame.transform.flip(self.image, True, False)
 
-        self.rect.x = self.pos_x + 16
-        self.rect.y = self.pos_y + 16
 
-    def check_collisions(self, tiles, lvl_w, lvl_h):
+
+    def update_rect(self, tiles, level_width, level_height):
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.rect.right > lvl_w:
-            self.rect.right = lvl_w
-        if self.rect.bottom > lvl_h:
-            self.rect.bottom = lvl_h
+        if self.rect.right > level_width:
+            self.rect.right = level_width
+        if self.rect.bottom > level_height:
+            self.rect.bottom = level_height
+
+        if abs(self.vel_x) > self.maxvel_x:
+            self.vel_x = self.maxvel_x if self.maxvel_x > 0 else -self.maxvel_x
+        if abs(self.vel_y) > self.maxvel_y:
+            self.vel_y = self.maxvel_y if self.maxvel_y > 0 else -self.maxvel_y
+
+        self.rect.x += self.vel_x * framerate
+        self.hitbox.midbottom = self.rect.midbottom
+        self.check_collisions(tiles, self.vel_x, 0)
+        self.rect.y += self.vel_y * framerate
+        self.hitbox.midbottom = self.rect.midbottom
+        self.check_collisions(tiles, 0, self.vel_y)
+
+
+
+    def check_collisions(self, tiles, vel_x, vel_y):
+        floor_collide = False
         for tile in tiles:
-            if pygame.sprite.collide_rect(self, tile):
-                if self.vel_x > 0:
+            if self.hitbox.colliderect(tile.rect):
+                if vel_x > 0:
                     self.vel_x = 0
-                    self.rect.right = tile.rect.left
-                if self.vel_x < 0:
+                    self.hitbox.right = tile.rect.left
+                    self.rect.midbottom = self.hitbox.midbottom
+                if vel_x < 0:
                     self.vel_x = 0
-                    self.rect.left = tile.rect.right
-                if self.vel_y > 0:
+                    self.hitbox.left = tile.rect.right
+                    self.rect.midbottom = self.hitbox.midbottom
+                if vel_y > 0:
                     self.vel_y = 0
-                    self.rect.bottom = tile.rect.top
+                    self.hitbox.bottom = tile.rect.top
+                    self.rect.midbottom = self.hitbox.midbottom
+                    self.status = PlayerStatus.idle
                     self.inair = False
-                    self.jumping = False
-                if self.vel_y < 0:
+                if vel_y < 0:
                     self.vel_y = 0
-                    self.rect.top = tile.rect.bottom
-            if tile.rect.collidepoint(self.rect.center[0], self.rect.bottom + 1):
+                    self.hitbox.top = tile.rect.bottom
+                    self.rect.midbottom = self.hitbox.midbottom
+            if (tile.rect.collidepoint(self.hitbox.left + 1, self.hitbox.bottom) or
+                tile.rect.collidepoint(self.hitbox.right - 2, self.hitbox.bottom)):
+                floor_collide = True
+        if not floor_collide:
+            self.inair = True
+
+
+
+    def idle(self):
+        self.status = PlayerStatus.idle
+        self.vel_x = 0
+        self.image = anim_idle
+
+    def walk(self, input_left, input_right):
+        if input_left and input_right:
+            self.flip = False
+            self.vel_x = 0
+        elif input_right:
+            self.vel_x = self.maxvel_x
+            self.flip = False
+            if not self.inair:
+                self.status = PlayerStatus.walk
+                self.image = anim_walk
+                anim_walk.play()
+        elif input_left:
+            self.vel_x = -self.maxvel_x
+            self.flip = True
+            if not self.inair:
+                self.status = PlayerStatus.walk
+                self.image = anim_walk
+                anim_walk.play()
+
+    def fall(self):
+        self.vel_y += 20
+        self.image = anim_jump
+
+    def jump(self, input_A):
+        if not self.inair:
+            if input_A:
+                self.status = PlayerStatus.jumpcharge
+                self.vel_x = 0
+                self.jumpcharge += 10
+                if self.jumpcharge > self.maxvel_y:
+                    self.jumpcharge = self.maxvel_y
+            elif self.jumpcharge >= 10:
+                self.status = PlayerStatus.jump
+                self.vel_y = -self.jumpcharge
+                self.jumpcharge = 0
                 self.inair = True
