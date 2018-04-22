08c1e9af0853efabc624d54716a5dec5b23f825b
Lexou <lexou-dit-tuc@hotmail.fr>
2018-04-22 16:46:58 +0200

    fuque

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from balls import *
-from pyganim import *
+import pyganim
 import spritesheet
 
 framerate = 1 / (1000/60)
@@ -7,15 +7,32 @@ framerate = 1 / (1000/60)
 alpha = (211, 249, 188) # 0xD3F9BC
 
 sheet = spritesheet.spritesheet("tiger.png")
-anim_idle = sheet.image_at((44,86,64,64), alpha)
-anim_jump = sheet.image_at((44,216,64,64), alpha)
-anim_walk = PygAnimation([
-            (sheet.image_at(( 44, 151, 64, 64), alpha), 0.1),
-            (sheet.image_at((109, 151, 64, 64), alpha), 0.1),
-            (sheet.image_at((174, 151, 64, 64), alpha), 0.1),
-            (sheet.image_at((239, 151, 64, 64), alpha), 0.1),
-            (sheet.image_at((304, 151, 64, 64), alpha), 0.1),
-            (sheet.image_at((369, 151, 64, 64), alpha), 0.1)])
+anim_idle = sheet.image_at((  1,  1,64,64), alpha)
+anim_walk = pyganim.PygAnimation([
+            (sheet.image_at((  1, 66, 64, 64), alpha), 0.1),
+            (sheet.image_at(( 66, 66, 64, 64), alpha), 0.1),
+            (sheet.image_at((131, 66, 64, 64), alpha), 0.1),
+            (sheet.image_at((196, 66, 64, 64), alpha), 0.1),
+            (sheet.image_at((261, 66, 64, 64), alpha), 0.1),
+            (sheet.image_at((326, 66, 64, 64), alpha), 0.1)])
+anim_jumpcharge = pyganim.PygAnimation([
+            (sheet.image_at((  1, 131, 64, 64), alpha), 0.2),
+            (sheet.image_at(( 66, 131, 64, 64), alpha), 0.2),
+            (sheet.image_at((131, 131, 64, 64), alpha), 0.2)])
+anim_jump = sheet.image_at((261,131,64,64), alpha)
+anim_fall = sheet.image_at((326,131,64,64), alpha)
+anim_golfcharge = pyganim.PygAnimation([
+            (sheet.image_at((  1, 196, 64, 64), alpha), 0.2),
+            (sheet.image_at(( 66, 196, 64, 64), alpha), 0.2),
+            (sheet.image_at((131, 196, 64, 64), alpha), 0.2),
+            (sheet.image_at((196, 196, 64, 64), alpha), 0.2),
+            (sheet.image_at((261, 196, 64, 64), alpha), 0.2),
+            (sheet.image_at((326, 196, 64, 64), alpha), 0.2)])
+anim_golf = pyganim.PygAnimation([
+            (sheet.image_at(( 1, 261, 64, 64), alpha), 0.1),
+            (sheet.image_at((66, 261, 64, 64), alpha), 0.1)])
+
+sfx_jump = pygame.mixer.Sound("sfx_jump.wav")
 
 from enum import Enum
 class PlayerStatus(Enum):
@@ -36,7 +53,7 @@ class Player(pygame.sprite.Sprite):
         self.hitbox.midbottom = self.rect.midbottom
         self.vel_x = 0.0
         self.vel_y = 0.0
-        self.maxvel_x = 30
+        self.maxvel_x = 35
         self.maxvel_y = 250
 
         self.status = PlayerStatus.idle
@@ -44,6 +61,8 @@ class Player(pygame.sprite.Sprite):
         self.flip = False
         self.inair = True
         self.jumpcharge = 0
+        self.golfcharge = 0
+        self.maxgolf = 70
 
         self.allow_jump = True
         self.allow_golf = True
@@ -60,18 +79,27 @@ class Player(pygame.sprite.Sprite):
 
         if self.status == PlayerStatus.damage:
             pass
-        elif input_up or input_down or input_left or input_right or input_A or input_B:
+        if self.status == PlayerStatus.jumpcharge:
+            self.jump(input_A)
+        if self.status == PlayerStatus.golfcharge:
+            self.golf(input_B)
+        elif input_left or input_right:
             self.walk(input_left, input_right)
+        elif input_A:
+        	self.jump(input_A)
+        elif input_B:
+        	self.golf(input_B)
         else:
             self.idle()
 
-        self.jump(input_A)
+        
+
         if self.inair:
             self.fall()
 
         self.update_rect(tiles, level_width, level_height)
 
-        if isinstance(self.image, PygAnimation):
+        if isinstance(self.image, pyganim.PygAnimation):
             #self.elapsed = self.image.elapsed
             self.image = self.image.getCurrentFrame()
         if self.flip:
@@ -135,16 +163,9 @@ class Player(pygame.sprite.Sprite):
         if input_left and input_right:
             self.flip = False
             self.vel_x = 0
-        elif input_right:
-            self.vel_x = self.maxvel_x
-            self.flip = False
-            if not self.inair:
-                self.status = PlayerStatus.walk
-                self.image = anim_walk
-                anim_walk.play()
-        elif input_left:
-            self.vel_x = -(self.maxvel_x / 2)
-            self.flip = True
+        else:
+            self.vel_x = self.maxvel_x if (input_right) else -self.maxvel_x
+            self.flip = input_left
             if not self.inair:
                 self.status = PlayerStatus.walk
                 self.image = anim_walk
@@ -152,22 +173,49 @@ class Player(pygame.sprite.Sprite):
 
     def fall(self):
         self.vel_y += 12
-        self.image = anim_jump
+        if self.vel_y <= 0:
+            self.image = anim_jump
+        else:
+            self.image = anim_fall
 
     def jump(self, input_A):
         if not input_A:
             self.allow_jump = True
         if not self.inair:
             if input_A:
-                self.status = PlayerStatus.jumpcharge
+                if self.status != PlayerStatus.jumpcharge:
+                    self.status = PlayerStatus.jumpcharge
+                    self.jumpcharge = 30
                 self.vel_x = 0
-                self.jumpcharge += 10
-                if self.jumpcharge > self.maxvel_y:
+                self.jumpcharge += 1
+                if self.jumpcharge < self.maxvel_y:
+                    self.image = anim_jumpcharge
+                    anim_jumpcharge.play()
+                else:
                     self.jumpcharge = self.maxvel_y
-            if ((self.jumpcharge >= self.maxvel_y) or
-                (self.jumpcharge >= 10 and not input_A)):
+                    anim_jumpcharge.stop()
+            if (self.jumpcharge >= self.maxvel_y):
                 self.status = PlayerStatus.jump
                 self.allow_jump = False
-                self.vel_y = -self.jumpcharge
+                self.vel_y = -(self.jumpcharge * 10)
                 self.jumpcharge = 0
                 self.inair = True
+
+    def golf(self, input_B):
+        if input_B:
+            self.status = PlayerStatus.golfcharge
+            self.vel_x = 0
+            self.golfcharge += 1
+            if self.golfcharge < self.maxgolf:
+                self.image = anim_golfcharge
+                anim_golfcharge.play()
+            else:
+                self.golfcharge = self.maxgolf
+                anim_golfcharge.stop()
+        elif self.golfcharge > 0:
+            self.status = PlayerStatus.golf
+            self.allow_golf = False
+            #ball.vel_y = -(self.golfcharge * 10)
+            self.golfcharge = 0
+        else:
+            self.allow_golf = True
