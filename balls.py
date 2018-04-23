a5c86811b453c8e039f15407ecbf53f925c92f68
fulguritude <duquesne.tristan@gmail.com>
2018-04-23 03:09:38 +0200

    minor change to pmain and ball

--- balls.py
+++ balls.py
@@ -1,8 +1,7 @@
-import pygame
 from entity import *
 
 class Ball(Entity):
-    def __init__(self, image, size, x, y):
+    def __init__(self, image, size, x, y, player):
         Entity.__init__(self, image, size, x, y)
         self.size = self.image.get_width
         self.maxvel_x = 0
@@ -10,6 +9,7 @@ class Ball(Entity):
         self.ang_mmt = 0 #positive is clockwise
         #self.rotations = self.build_rotations #wtf dude
         self.cur_orient = 0
+        self.player = player
 
         self.hasMomentum = False
         self.isRolling = False
@@ -18,11 +18,11 @@ class Ball(Entity):
     def build_rotations(self):
         img = self.image
         rots = self.size
-        incr = 360 / rots;
+        incr = 360 / rots
         res = []
         res.append(img)
         for i in range(1, rots):
-            res.append(transform.rotate(img, i * incr))
+            res.append(pygame.transform.rotate(img, i * incr))
         return res
 
     def fall(self):
@@ -37,17 +37,20 @@ class Ball(Entity):
         self.vel_y += yvel
 
     def update(self):
-        if (self.inair):
+        global tiles
+        if self.inair:
             self.fall()
         else:
             self.idle()
-        self.update_rect(tiles, level_width, level_height)
+        self.update_rect(tiles)
 
 class Bomb(Ball):
     def __init__(self, x, y):
+        global img_bomb
         Ball.__init__(self, img_bomb, 16, x, y)
         self.exploding = True
 
 class Pebble(Ball):
     def __init__(self, x, y):
+        global img_pebble
         Ball.__init__(self, img_pebble, 8, x, y)
